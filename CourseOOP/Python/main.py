bd41903222947b9fbc57a83db2dd6e3e6db5d910
yepedraza <yery.pedraza523@gmail.com>
2022-02-25 17:07:45 -0500

    Adding constructor methods

--- CourseOOP/Python/main.py
+++ CourseOOP/Python/main.py
@@ -1,16 +1,15 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print('Hola mundo')
 
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Yery Pedraza"
-    car.passenger = 4
+    car = Car("AMS234", Account("Yery Pedraza", "ABC123"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Agusto Agudelo"
-    car2.passenger = 3
-    print(vars(car2))
+    # car2 = Car()
+    # car2.license = "QWE567"
+    # car2.driver = "Agusto Agudelo"
+    # car2.passenger = 3
+    # print(vars(car2))
