0d486b501768c22b2ea5a22da6890353d39295e3
yepedraza <yery.pedraza523@gmail.com>
2022-02-24 19:34:03 -0500

    created and use object car

--- CourseOOP/Python/main.py
+++ CourseOOP/Python/main.py
@@ -1,2 +1,16 @@
+from car import Car
+
 if __name__ == "__main__":
     print('Hola mundo')
+
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Yery Pedraza"
+    car.passenger = 4
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Agusto Agudelo"
+    car2.passenger = 3
+    print(vars(car2))
