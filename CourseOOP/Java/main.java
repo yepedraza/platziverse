26ca89b732f76e4828d04f34024cdaeec5076db4
yepedraza <yery.pedraza523@gmail.com>
2022-02-24 19:20:55 -0500

    Object car created and print example

--- CourseOOP/Java/main.java
+++ CourseOOP/Java/main.java
@@ -3,6 +3,20 @@ package Java;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        car car = new car();
+        car.license = "AMQ123";
+        car.driver = "Yery Pedraza";
+        car.passenger = 4;
+
+        System.out.println("Car License: " + car.license);
+
+        car car2 = new car();
+        car2.license = "QWE567";
+        car2.driver = "Agusto Agudelo";
+        car2.passenger = 3;
+
+        System.out.println("Car License: " + car2.license);
     }
 
 }
