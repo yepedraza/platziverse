b86e8e1cd6e7fe9f3230497bb3539396bad25913
yepedraza <yery.pedraza523@gmail.com>
2022-02-26 10:43:50 -0500

    adding uberx use

--- CourseOOP/Python/main.py
+++ CourseOOP/Python/main.py
@@ -1,12 +1,17 @@
+from UberX import UberX
+from normalService import NormalService
 from car import Car
 from account import Account
 
 if __name__ == "__main__":
     print('Hola mundo')
 
-    car = Car("AMS234", Account("Yery Pedraza", "ABC123"))
-    print(vars(car))
-    print(vars(car.driver))
+    # car = Car("AMS234", Account("Yery Pedraza", "ABC123"))
+    # print(vars(car))
+    # print(vars(car.driver))
+
+    uberX = UberX("TYU159", Account("Agusto Pedraza", "AW123"), "Subaru", "Impreza")
+    print(vars(uberX))
 
     # car2 = Car()
     # car2.license = "QWE567"
