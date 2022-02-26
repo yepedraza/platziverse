1a8974cf99a332efe3e9b7a5ef4d2c139ca085ef
yepedraza <yery.pedraza523@gmail.com>
2022-02-26 11:26:47 -0500

    Added inheritance to NormalService

--- CourseOOP/Java/UberPool.java
+++ CourseOOP/Java/UberPool.java
@@ -1,12 +1,8 @@
 package Java;
 
-public class UberPool extends car {
-    String brand;
-    String model;
+public class UberPool extends NormalService {
 
     public UberPool(String license, account driver, String brand, String model){
-        super(license, driver);
-        this.brand = brand;
-        this.model = model;
+        super(license, driver, brand, model);
     }
 }
--- CourseOOP/Java/UberX.java
+++ CourseOOP/Java/UberX.java
@@ -1,12 +1,8 @@
 package Java;
 
-public class UberX extends car {
-    String brand;
-    String model;
+public class UberX extends NormalService {
 
     public UberX(String license, account driver, String brand, String model){
-        super(license, driver);
-        this.brand = brand;
-        this.model = model;
+        super(license, driver, brand, model);
     }
 }
