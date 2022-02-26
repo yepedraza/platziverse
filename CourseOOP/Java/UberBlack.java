985bbe6e6543591cee4519a447fd140ae81cd531
yepedraza <yery.pedraza523@gmail.com>
2022-02-26 11:26:55 -0500

    Added inheritance to EspecialService

--- CourseOOP/Java/UberBlack.java
+++ CourseOOP/Java/UberBlack.java
@@ -3,15 +3,11 @@ package Java;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class UberBlack extends car {
-    Map<String, Map<String, Integer>> typeCarAccepted;
-    ArrayList<String> seatsMaterial;    
+public class UberBlack extends EspecialService {   
 
     public UberBlack(String license, account driver, 
     Map<String, Map<String, Integer>> typeCarAccepted, 
     ArrayList<String> seatsMaterial){
-        super(license, driver);
-        this.typeCarAccepted = typeCarAccepted;
-        this.seatsMaterial = seatsMaterial;
+        super(license, driver, typeCarAccepted, seatsMaterial);
     }
 }
