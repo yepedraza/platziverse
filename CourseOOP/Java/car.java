fdcaf0a8625d68cf1a389a6daa34af5a58d1ac55
yepedraza <yery.pedraza523@gmail.com>
2022-02-26 12:24:22 -0500

    Adding setters and getters

--- CourseOOP/Java/car.java
+++ CourseOOP/Java/car.java
@@ -1,10 +1,10 @@
 package Java;
 
 public class car {
-    Integer id;
-    String license;
-    account driver;
-    Integer passenger;
+    private Integer id;
+    private String license;
+    private account driver;
+    private Integer passenger;
 
     public car(String license, account driver){
         this.license = license;
@@ -12,6 +12,45 @@ public class car {
     }
 
     void printDataCar(){
-        System.out.println("Car License: " + license + " Name Driver: " + driver.name);
+        System.out.println("Car License: " + license + " Name Driver: " + driver.name + " Passengers: " + passenger);
     }
+
+    public Integer getPassenger(){
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger){
+        if(passenger == 4){
+            this.passenger = passenger;
+        }
+        else{
+            System.out.println("Data dennied!");
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(account driver) {
+        this.driver = driver;
+    }
+
+    
 }
--- CourseOOP/Java/main.java
+++ CourseOOP/Java/main.java
@@ -2,19 +2,18 @@ package Java;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
 
         car car = new car("AMQ123", new account("Yery Pedraza", "AMQ123"));
-        car.passenger = 4;
+        //car.passenger = 4;
 
         // System.out.println("Car License: " + car.license); 
         car.printDataCar(); //Using print method
 
-        car car2 = new car("QWE567", new account("Agusto Agudelo", "QWE567"));
-        car2.passenger = 3;
+        UberX uberX = new UberX("QWE567", new account("Agusto Agudelo", "QWE567"), "Chevrolet", "Sonic");
+        uberX.setPassenger(3);
 
         //System.out.println("Car License: " + car2.license);
-        car2.printDataCar(); //Using print method
+        uberX.printDataCar(); //Using print method
     }
 
 }
