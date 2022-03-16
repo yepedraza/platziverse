18cb2f7218273d57b40df32ec13fab01a4997e3f
yepedraza <yery.pedraza523@gmail.com>
2022-03-16 11:01:07 -0500

    Deleting extra import

--- CourseOOP/Py_AlgorithmsCourse/ex_bubbleSort.py
+++ CourseOOP/Py_AlgorithmsCourse/ex_bubbleSort.py
@@ -1,12 +1,11 @@
 import random
-from socket import if_nameindex
 
 def BubbleSort(list):
 
     n = len(list)
 
     for i in range(n):
-        for j in range(0, n - i - 1): # O(n) * O(n) = O(n**2)
+        for j in range(0, n - i - 1): # O(n) * O(n) = O(n**2) 
 
             if list[j] > list [j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
