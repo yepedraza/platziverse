2d91aba86ff690255e1db6487f477caeab3922b5
yepedraza <yery.pedraza523@gmail.com>
2022-03-27 11:48:09 -0500

    Added a max parameter

--- ProfessionalPy/fibonacciSequence/fibonacci.py
+++ ProfessionalPy/fibonacciSequence/fibonacci.py
@@ -2,6 +2,9 @@ import time
 
 class FiboIter():
 
+    def __init__(self, max=None) -> None:
+        self.max = max
+
     def __iter__(self):
         self.n1 = 0
         self.n2 = 1
@@ -10,23 +13,27 @@ class FiboIter():
         return self
 
     def __next__(self):
-        if self.counter == 0:
-            self.counter =+ 1
-            return self.n1
-        elif self.counter == 1:
-            self.counter += 1
-            return self.n2
+
+        if not self.max or self.counter <= self.max:
+            if self.counter == 0:
+                self.counter =+ 1
+                return self.n1
+            elif self.counter == 1:
+                self.counter += 1
+                return self.n2
+            else:
+                self.aux = self.n1 + self.n2
+                #self.n1 = self.n2
+                #self.n2 = self.aux
+                self.n1, self.n2 = self.n2, self.aux # Line 21 & 22 are resumed in this line -> It's called swapping
+                self.counter += 1
+                return self.aux
         else:
-            self.aux = self.n1 + self.n2
-            #self.n1 = self.n2
-            #self.n2 = self.aux
-            self.n1, self.n2 = self.n2, self.aux # Line 21 & 22 are resumed in this line -> It's called swaping
-            self.counter += 1
-            return self.aux
+            raise StopIteration
 
 if __name__ == '__main__':
 
-    fibonacci = FiboIter()
+    fibonacci = FiboIter(4)
 
     for element in fibonacci:
         print(element)
