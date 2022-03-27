6c02f08179a1016f193869b1d0b436663878bcff
yepedraza <yery.pedraza523@gmail.com>
2022-03-27 12:13:37 -0500

    Added max parameter

--- ProfessionalPy/fibonacciSequence/fibonacci_g.py
+++ ProfessionalPy/fibonacciSequence/fibonacci_g.py
@@ -1,26 +1,26 @@
 import time
 
-def FiboGen():
+def FiboGen(max=None):
     n1 = 0
     n2 = 1
     counter = 0
 
-    while True:
-        if counter == 0:
-            counter += 1
-            yield n1
-        elif counter == 1:
-            counter += 1
-            yield n2
-        else:
-            aux = n1 + n2
-            n1, n2 = n2, aux
-            counter += 1
-            yield aux
+    while not max or counter <= max:
+            if counter == 0:
+                counter += 1
+                yield n1
+            elif counter == 1:
+                counter += 1
+                yield n2
+            else:
+                aux = n1 + n2
+                n1, n2 = n2, aux
+                counter += 1
+                yield aux
 
 def run():
     
-    fibonacci = FiboGen()
+    fibonacci = FiboGen(4)
     for element in fibonacci:
         print(element)
         time.sleep(1)
